74b4b33f6d73369d433c838981eb84fa7aabc083
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2020-12-31 15:16:58 -0500

    Update Pedigree_format.R

--- Pedigree_format.R
+++ Pedigree_format.R
@@ -61,8 +61,7 @@ Landracepedigree.file<- select(Landracepedigree.file, "Sex", "Animal#", "Birthda
 Landracepedigree.file<- mutate(Landracepedigree.file, Sire)
 Landracepedigree.file<- mutate(Landracepedigree.file, Dam)
 
-Landracepedigree.file<- select(Landracepedigree.file, "Sex", "Animal#", "Birthdate", "Animal_Name")
-Landracepedigree.file<- select(Landracepedigree.file, "Sex", "Sire", "Animal#", "Dam", "Birthdate", "Animal_Name")
+Landracepedigree.file<- select(Landracepedigree.file, "Sex","Sire", "Animal#", "Dam", "Birthdate", "Animal_Name")
 
 Write.table(Landracepedigree.file, file= "LandracePedigree.txt", append=FALSE, quote=FALSE, sep= " ", row.names= FALSE, col.names= TRUE)
 
