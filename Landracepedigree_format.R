639ac990d201fa9f1a82a5e3815e7293e7019b3f
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-01-06 08:24:54 -0500

    Update and rename Pedigree_format.R to Landracepedigree_format.R

--- Landracepedigree_format.R
+++ Landracepedigree_format.R
@@ -91,21 +91,3 @@ Landracepedigree.file<- select(Landracepedigree.file, "Sex", "Animal_Num", "Sire
     
 
 write.table(Landracepedigree.file, file= "Landracepedigree.txt", append=FALSE, quote=FALSE, sep= " ", row.names= FALSE, col.names= TRUE)
-
-***********************************************************************************************************************************************************************************
-
-#Will convert Yorkshire pedigree file in same way as Landrace 
-Yorkshirepedigree.file<- read.table("pedigreeYorkshire.txt")
-
-colnames(Yorkshirepedigree.file)<- c("Animal_ID", "Sire", "Dam", "Birthday", "Sex")
-
-Yorkshirepedigree.file<- Yorkshirepedigree.file[order(x$Birthday),]
-Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, y)
-Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, z)
-Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal#", "Sire", "Dam", "Birthday", "y", "z")
-
-colnames(Yorkshirepedigree.file) <- c("Sex", "Animal#", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
-
-write.table(Yorkshirepedigree.file, file= "YorkshirePedigree.txt", append=FALSE, quote=FALSE, sep= " ", row.names= FALSE, col.names= TRUE)
-
-#Convert Animal#, Animal_ID, and Animal_Name into numeric form
