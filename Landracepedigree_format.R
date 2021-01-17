3fa03cd79b137521a6f29ea417975ec0d35ac49f
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-01-17 18:42:59 -0500

    Update Landracepedigree_format.R

--- Landracepedigree_format.R
+++ Landracepedigree_format.R
@@ -73,7 +73,7 @@ Landracepedigree.file<- select(Landracepedigree.file, "Sex", "Animal#", "Birthda
 Landracepedigree.file<- mutate(Landracepedigree.file, Sire)
 Landracepedigree.file<- mutate(Landracepedigree.file, Dam)
 
-Landracepedigree.file<- select(Landracepedigree.file, "Sex","Animal_Num", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
+Landracepedigree.file<- select(Landracepedigree.file, "Sex","Animal#", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
 
 #Remove and Replace Animal# column with numeric 
 
