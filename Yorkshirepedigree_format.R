36cebb4640fdb07e71c7e332b76237e22bb04069
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-01-18 14:25:04 -0500

    Update Yorkshirepedigree_format.R

--- Yorkshirepedigree_format.R
+++ Yorkshirepedigree_format.R
@@ -1,7 +1,20 @@
-#Will convert Yorkshire pedigree file in same way as Landrace 
+#FindHap is used to impute missing genotypes in the genotype files for Landrace and Yorkshire swine breeds 
+#Before running FindHap, we must make sure that our data formats align appropriately using R. 
 
-#Yorkshire Pedigree file must be ordered in ascending order of birthday; oldest to youngest animal;
-Yorkshirepedigree.file<- read.table("Yorkshirepedigree.txt") 
+#Landrace Pedigree File file format conversion
+
+#Set CRAN repository 
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+
+#Install dplyr package
+install.packages("dplyr")
+library(dplyr)
+
+        
+#Landrace Pedigree file must be ordered in ascending order of birthday; oldest to youngest animal;
+Yorkshirepedigree.file<- read.table("YorkshirePedigree.txt") 
 
 #Check names of columns with: 
 colnames(Yorkshirepedigree.file)
@@ -13,16 +26,13 @@ colnames(Yorkshirepedigree.file)<- c("Animal_ID", "Sire", "Dam", "Birthdate", "S
 #Order the Landrace Pedigree file from oldest animal to youngest animal: 
 Yorkshirepedigree.file<- Yorkshirepedigree.file[order(Yorkshirepedigree.file$Birthdate),]
 
-#Install dplyr package
-install.packages("dplyr")
-library(dplyr)
 
 #Add on two duplicate columns of Animal_ID to serve as Animal# and Animal_Name
-Animal_ID<- pull(Landracepedigree.file, Animal_ID)
+Animal_ID<- pull(Yorkshirepedigree.file, Animal_ID)
 y<- Animal_ID
 z<- Animal_ID
-Yorkshirepedigree.file<- mutate(Landracepedigree.file, y)
-Yorkshirepedigree.file<- mutate(Landracepedigree.file, z)
+Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, y)
+Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, z)
 
 #Reorder columns in format that corresponds to findhap.f90 
 Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal_ID", "Sire", "Dam", "Birthdate", "y","z")
@@ -35,7 +45,7 @@ Animal_ID<- as.factor(as.character(Animal_ID))
 Animal_ID<- as.numeric(as.factor(Animal_ID))
 
 #Remove current character columns to replace with new numeric columns w "select" command
-Yorkshirepedigree.file<- select(Landracepedigree.file, "Sex", "Sire", "Dam", "Birthdate")
+Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Sire", "Dam", "Birthdate")
 
 #Recreate the Animal_ID y and z variable with the new numeric variable with: 
 y<- Animal_ID
@@ -45,7 +55,7 @@ Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, Animal_ID)
 Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, y) 
 Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, z)
 
-colnames(Yorkshirepedigree.file)<- c("Sex", "Animal#", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
+colnames(Yorkshirepedigree.file)<- c("Sex", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal#", "Animal_Name")
 
 #Convert Sire and Dam names from character to numeric in same manner 
 Sire<- pull(Yorkshirepedigree.file, Sire)
@@ -63,22 +73,22 @@ Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal#", "Birth
 Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, Sire)
 Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, Dam)
 
-Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex","Animal_Num", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
+Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal#", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
 
 #Remove and Replace Animal# column with numeric 
-
 Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex","Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
 
 Animal_Num<- Animal_ID 
 Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, Animal_Num)
                                
-Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal_Num", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name") 
+Yorkhirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal_Num", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name") 
 
 #Convert 0,1,2 to F,M F respectively 
 Sex<- pull(Yorkshirepedigree.file, Sex)
-class(Sex)= integer; #Must change to factor and assign levels accordingly 
+
+#Must change to factor and assign levels accordingly 
 Sex<- as.factor(as.integer(Sex))
-levels(Sex)= 0,1,2
+levels(Sex)= c("0","1","2")
 levels(Sex)<- c("F", "M", "F")
 
 #Remove existing Sex column and replace with factored version 
@@ -87,5 +97,4 @@ Yorkshirepedigree.file<- mutate(Yorkshirepedigree.file, Sex)
 Yorkshirepedigree.file<- select(Yorkshirepedigree.file, "Sex", "Animal_Num", "Sire", "Dam", "Birthdate", "Animal_ID", "Animal_Name")
     
 
-
-write.table(Yorkshirepedigree.file, file= "YorkshirePedigree.txt", append=FALSE, quote=FALSE, sep= " ", row.names= FALSE, col.names= TRUE)
+write.table(Yorkshirepedigree.file, file= "Yorkshirepedigree.txt", append=FALSE, quote=FALSE, sep= " ", row.names= FALSE, col.names= TRUE)
