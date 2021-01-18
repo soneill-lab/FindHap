2e9dd4baeace3225d50752a1d7df4e4ec63b5a97
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-01-18 15:07:26 -0500

    Update and rename Chromosome_data_formatting.R to Chromosome.data_formatting.R

--- Chromosome.data_formatting.R
+++ Chromosome.data_formatting.R
@@ -52,7 +52,7 @@ chromosome.data$within <- sequence(rle(chromosome.data$chrome)$lengths)
 chromosome.data<- select(chromosome.data, "SNPname", "chrome", "within","Overall", "location", "chip1")
 
 #Save file 
-write.table(chromosome.data, file= chromosome.data, append=FALSE, quote=FALSE, sep=" ", row.names=FALSE, col.names=TRUE)
+write.table(chromosome.data, file= "chromosome.data", append=FALSE, quote=FALSE, sep=" ", row.names=FALSE, col.names=TRUE)
 
 
 
