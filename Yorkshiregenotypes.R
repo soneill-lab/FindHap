cf846ec67d46f51cbf92188e81fe6472d067f684
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-01-19 12:13:46 -0500

    Update Yorkshiregenotypes.R

--- Yorkshiregenotypes.R
+++ Yorkshiregenotypes.R
@@ -2,19 +2,25 @@ install.packages("dplyr")
 library(dplyr)
 install.packages("data.table")
 library(data.table)
-###############################
+
+
 Ygenotypes<-fread("genotypeYorkshire.txt)
 names(Ygenotypes)<- c("An_ID", "Genotypes")
-###############################
+
+
 An_ID<- pull(Ygenotypes, An_ID)
 Genotypes<- pull(Ygenotypes, Genotypes)
-###############################
+
+
 chip<- 1 
 SNP_num<- 5000
-###############################
+
+
 Ygenotypes<- mutate(Ygenotypes, chip1)
 Ygenotypes<- mutate(Ygenotypes, SNP_num)
-###############################
+
+
 Ygenotypes<- select(Ygenotypes, "AN_ID", "chip", "SNP_num", "Genotypes")
-###############################
+
+
 write.table(Ygenotypes, file= "Yorkshiregenotypes.txt", append=FALSE, sep=" ", row.names=FALSE, col.names=TRUE) 
