40c3da4cd4820a60b15e08da6d42ad713fc6768d
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-01-20 10:02:32 -0500

    Update Yorkshiregenotypes.R

--- Yorkshiregenotypes.R
+++ Yorkshiregenotypes.R
@@ -9,23 +9,23 @@ install.packages("data.table")
 library(data.table)
 
 
-Ygenotypes<-fread("genotypeYorkshire.txt)
+Ygenotypes<- fread("genotypeYorkshire.txt)
 names(Ygenotypes)<- c("An_ID", "Genotypes")
 
 
-An_ID<- pull(Ygenotypes, An_ID)
-Genotypes<- pull(Ygenotypes, Genotypes)
+An_ID<- pull(Ygenotypes, "An_ID")
+Genotypes<- pull(Ygenotypes, "Genotypes")
 
 
 chip<- 1 
 SNP_num<- 5000
 
 
-Ygenotypes<- mutate(Ygenotypes, chip1)
+Ygenotypes<- mutate(Ygenotypes, chip)
 Ygenotypes<- mutate(Ygenotypes, SNP_num)
 
 
-Ygenotypes<- select(Ygenotypes, "AN_ID", "chip", "SNP_num", "Genotypes")
+Ygenotypes<- select(Ygenotypes, "An_ID", "chip", "SNP_num", "Genotypes")
 
 
 write.table(Ygenotypes, file= "Yorkshiregenotypes.txt", append=FALSE, sep=" ", row.names=FALSE, col.names=TRUE) 
