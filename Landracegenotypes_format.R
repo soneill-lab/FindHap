60e142775b60294ed01e2ba134f5d44c833b6bed
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-02-01 14:25:16 -0500

    Update Landracegenotypes_format.R

--- Landracegenotypes_format.R
+++ Landracegenotypes_format.R
@@ -21,6 +21,12 @@ names(genotypes)<- c("An_ID", "Genotypes")
 An_ID<- pull(genotypes, An_ID)
 Genotypes<- pull(genotypes, Genotypes) 
 
+An_ID<- as.factor(as.character(An_ID))
+An_ID<- as.numeric(as.factor(An_ID))
+genotypes<- select(genotypes,"SNP_num", "Genotypes")
+genotypes<- mutate(genotypes, An_ID)
+
+genotypes<- select(genotypes, "An_ID","SNP_num", "Genotypes")
 
 chip<- 1 
 SNP_num<- 5000
@@ -30,7 +36,7 @@ genotypes<- mutate(genotypes, chip)
 genotypes<- mutate(genotypes, SNP_num)
 
 
-genotypes<- select(genotypes, "An_ID", "chip", "SNP_num", "Genotypes")
+
 
 
 write.table(genotypes, file= "Landracegenotypes.txt", append=FALSE, sep=" ", row.names=FALSE, col.names=TRUE)
