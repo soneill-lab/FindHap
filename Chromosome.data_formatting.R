e9754770334593d6e47b4baddfe557f6985ca21d
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-02-07 17:58:44 -0500

    Update Chromosome.data_formatting.R

--- Chromosome.data_formatting.R
+++ Chromosome.data_formatting.R
@@ -17,14 +17,21 @@ colnames(chromosome.data)<- c("chrome", "SNPname", "location")
 location<- pull(chromosome.data, location)
 location=as.numeric(location)
 
+#Make chrome a variable
+chrome<- chromosome.data$chrome
+
 #Remove and add back proper location column
 chromosome.data<- select(chromosome.data, "SNPname", "chrome")
 chromosome.data<- mutate(chromosome.data, location)
 
-#Sort position and chromosome number in ascending order 
-list(chromosome.data, sort("location"))
-list(chromosome.data, sort("chrome"))
+#Sort position and chromosome number in ascending order
+chromosome.data<- chromosome.data[ order(chromosome.data[ ,"chrome"], chromosome.data[ ,"location"] ),]
 
+#Create overall as a variabl
+overall<- (1:49991)
+
+#Add overall column
+chromosome.data<- mutate(chromosome.data, overall)                                   
 #Rearrange order of columns to appropriate format 
  
 #Rename SNPname column to "SNP(number)"
@@ -33,8 +40,8 @@ SNPname<- as.factor(SNPname)
 SNPname<- as.numeric(as.factor(SNPname))
 
 #Remove current character SNPname column to make room to add the numeric SNPname column
-chromosome.data<- select(chromosome.data, "chrome", "location")
 
+chromosome.data<- select(chromosome.data, "chrome","location", "overall")
 #Add numeric SNPname column
 chromosome.data<- mutate(chromosome.data, SNPname)
 
@@ -47,12 +54,6 @@ chromosome.data<- mutate(chromosome.data, chip1)
 chromosome.data<- mutate(chromosome.data, n_chips)
  
 
-#Create overall as a variable 
-overall<- rownames(chromosome.data)
-
-#Add overall column
-chromosome.data<- mutate(chromosome.data, overall)
-
 #Create "within" as a particular sequence of numbers that will change dependent on chrome # 
 chromosome.data$within <- sequence(rle(chromosome.data$chrome)$lengths)
 
@@ -60,7 +61,7 @@ chromosome.data$within <- sequence(rle(chromosome.data$chrome)$lengths)
 chromosome.data<- select(chromosome.data, "SNPname", "chrome", "within","overall", "location", "n_chips", "chip1")
 
 #Save file 
-write.table(chromosome.data, file= "chromosome.data", append=FALSE, quote=FALSE, sep=" ", row.names=FALSE, col.names=TRUE)
+write.table(chromosome.data,file= "chromosome.data2",append=FALSE, quote=FALSE, sep=" ", row.names=FALSE, col.names=TRUE)
 
 
 
