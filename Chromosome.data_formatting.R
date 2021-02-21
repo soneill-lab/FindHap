eabca8ce1c5e2aa8586d44918044e839a49c6c8e
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-02-20 20:07:46 -0500

    Update Chromosome.data_formatting.R

--- Chromosome.data_formatting.R
+++ Chromosome.data_formatting.R
@@ -26,7 +26,7 @@ chromosome.data<- mutate(chromosome.data, location)
 
 #Sort position and chromosome number in ascending order
 ordered_idx = order(chromosome.data[ ,"chrome"], chromosome.data[ ,"location"] )
-chromosome.data<- chromosome.data[ ordered_idx,]
+chromosome.data<- chromosome.data[ordered_idx,]
 
 #Create overall as a variabl
 overall<- (1:49991)
