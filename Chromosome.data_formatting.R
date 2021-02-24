a25438294e7190bec71037bed4e7396e68332313
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-02-24 07:35:35 -0500

    Update Chromosome.data_formatting.R

--- Chromosome.data_formatting.R
+++ Chromosome.data_formatting.R
@@ -29,7 +29,7 @@ ordered_idx = order(chromosome.data[ ,"chrome"], chromosome.data[ ,"location"] )
 chromosome.data<- chromosome.data[ordered_idx,]
 
 #Create overall as a variabl
-overall<- (1:49991)
+overall<- (1:49992)
 
 #Add overall column
 chromosome.data<- mutate(chromosome.data, overall)                                   
