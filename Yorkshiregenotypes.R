777ef69f57d88e06dbfadbcbad7671fffaf1d094
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-04-06 10:04:54 -0400

    Update Yorkshiregenotypes.R

--- Yorkshiregenotypes.R
+++ Yorkshiregenotypes.R
@@ -14,7 +14,7 @@ genotypes_Animal_ID<- genotypes_Animal_ID[- remove_genotypes_idx]
 genotypes<- genotypes[-c(remove_genotypes_idx),]
 
 ped= data.frame(numeric= c(Animal_ID_ped_char), character=c(Animal_ID_ped_num))
-gen= c(genotypes_Animal_ID_char)
+gen= c(genotypes_Animal_ID)
 
 h= hash(ped$character, ped$numeric)
 genotypes_numeric_idx<- values(h, keys=gen)
