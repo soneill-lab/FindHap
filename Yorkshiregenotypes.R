a3f19e0448377a972317a6269f1393b4c6a59699
soneill-lab <75035355+soneill-lab@users.noreply.github.com>
2021-04-06 11:35:48 -0400

    Update Yorkshiregenotypes.R

--- Yorkshiregenotypes.R
+++ Yorkshiregenotypes.R
@@ -33,7 +33,7 @@ genotypes<- select(genotypes,"Genotypes")
 
 genotypes<- mutate(genotypes, chip)
 genotypes<- mutate(genotypes, SNP_num)
-genotypes<-mutate(genotypes, genotypes_numeric_idx)
+
 
 
 genotypes<- select(genotypes, "genotypes_numeric_idx", "chip", "SNP_num")
